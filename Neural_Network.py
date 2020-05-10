afb3f973fbacdf8a992bbc117315670c89b0eb5f
Alex <alex_15_96@live.com.mx>
2020-05-10 18:30:50 -0500

    Neural net

--- Neural_Network.py
+++ Neural_Network.py
@@ -9,14 +9,15 @@ class NeuralLayer:
     # Receive number of connections, number of neurons and activation function
     def __init__(self, n_connections, n_neurons, act_fun):
         self.act_fun = act_fun
-        self.bias = np.random.rand(1, n_neurons) * 2 - 1  # Generate a random vector with values from -1 to 1
+        # self.bias = np.random.rand(1, n_neurons) * 2 - 1  # Generate a random vector with values from -1 to 1
+        self.bias = np.ones([1, n_neurons])     # Generate a vector of one for bias
         self.weight = np.random.rand(n_connections, n_neurons) * 2 - 1  # Initialize the weights with random values
 
 
 # Create Neural Network
 def create_nn(_topology, act_fun):
     nn_layers = []
-    for l, layer in enumerate(_topology[:-1]):
+    for l in range(len(_topology)-1):
         nn_layers.append(NeuralLayer(_topology[l], _topology[l + 1], act_fun))
     return nn_layers
 
@@ -24,31 +25,34 @@ def create_nn(_topology, act_fun):
 # Train function
 def train(_neural_net, x, y, cost_fun, learn_r=0.05, _train=False):
     # forward pass
-    out = [(None, x)]
-    for l, layer in enumerate(_neural_net):
-        z = out[-1][1] @ _neural_net[l].weight + _neural_net[l].bias
-        a = _neural_net[l].act_fun[0](z)
+    out = list([x])
 
-        out.append((z, a))
+    for layer in range(len(_neural_net)):
+        z = np.dot(out[-1], _neural_net[layer].weight) + _neural_net[layer].bias
+        a = _neural_net[layer].act_fun[0](z)
+
+        out.append(a)
 
     if _train:
         # Backward pass (Back propagation)
         deltas = []
-        for l in reversed(range(0, len(_neural_net))):
-            z = out[l + 1][0]
-            a = out[l + 1][1]
+        for layer in reversed(range(len(_neural_net))):
+            a = out[layer + 1]
 
-            if l == len(_neural_net) - 1:
-                deltas.insert(0, cost_fun[1](a, y) * _neural_net[l].act_fun[1](a))
+            if layer == len(_neural_net) - 1:
+                # Delta Last layer
+                deltas.insert(0, cost_fun[1](a, y) * _neural_net[layer].act_fun[1](a))
             else:
-                deltas.insert(0, deltas[0] @ _W.T * _neural_net[l].act_fun[1](a))
+                # Delta with respect to previous layer
+                deltas.insert(0, np.dot(deltas[0], _W.T) * _neural_net[layer].act_fun[1](a))
 
-            _W = _neural_net[l].weight
+            _W = _neural_net[layer].weight
 
             # Gradient decent
-            _neural_net[l].bias = _neural_net[l].bias - np.mean(deltas[0], axis=0, keepdims=True) * learn_r
-            _neural_net[l].weight = _neural_net[l].weight - out[l][1].T @ deltas[0] * learn_r
-    return out[-1][1]
+            _neural_net[layer].bias = _neural_net[layer].bias - (np.mean(deltas[0], axis=0, keepdims=True) * learn_r)
+            _neural_net[layer].weight = _neural_net[layer].weight - (np.dot(out[layer].T, deltas[0]) * learn_r)
+
+    return out[-1]
 
 
 # Activation Functions
@@ -61,7 +65,7 @@ e2_mean = (lambda yp, yr: np.mean((yp - yr) ** 2),
 
 if __name__ == "__main__":
     # Create Dataset
-    n = 250  # Dates
+    n = 200  # Dates
     p = 2  # Characteristics
 
     X, Y = make_circles(n_samples=n, factor=0.5, noise=0.09)  # Generate the dataset
@@ -71,12 +75,13 @@ if __name__ == "__main__":
     neural_net = create_nn(topology, sigmoid)
 
     loss = []
-    for i in range(1000):
-        pY = train(neural_net, X, Y, e2_mean, learn_r=0.09, _train=True)
+    for i in range(500):
+        pY = train(neural_net, X, Y, e2_mean, learn_r=0.099, _train=True)
 
         if i % 5 == 0:
-            # print(pY)
+            #print(pY)
             loss.append(e2_mean[0](pY, Y))
+            print(i, ": ",loss[-1])
 
             res = 60
 
